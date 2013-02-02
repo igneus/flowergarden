c2d21bea58afb50799a65ae1b581f33a1f7e715c
Jakub Pavlík jn <severus@post.cz>
2013-02-02 16:39:26 +0100

    the homepage shows recent bouquets

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,12 @@
 import django.shortcuts
 from users.models import UserProfile
+from flowers.models import Bouquet
 
 def home(request):
     """
     Homepage of the website.
     """
     newcomers = UserProfile.objects.all() # todo: fake implementation!
-    return django.shortcuts.render_to_response('homepage.html', {'new_users': newcomers})
+    bouquets = Bouquet.objects.all() # todo: fake implementation!
+    return django.shortcuts.render_to_response('homepage.html', {'new_users': newcomers,
+                                                                 'new_bouquets': bouquets})
